00ae186ab23270e4098f205f0524357b42319efe
leexm <807415781@qq.com>
2020-09-27 01:27:24 +0800

    服务消费者

---
+++ service-consumer/src/main/java/com/leexm/demo/consumer/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.leexm.demo.consumer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author leexm
+ * @date 2020-09-27 01:08
+ */
+@RestController
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/hello")
+    public String hello() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("hello-service");
+        String url = String.format("http://%s:%d/hello", serviceInstance.getHost(), serviceInstance.getPort());
+        logger.info("URL:" + url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
